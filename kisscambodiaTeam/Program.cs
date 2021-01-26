2d0aa532d2085e46c49d4112605974a4faefc788
long charmroeun <longcharmroeun@gmail.com>
2021-01-26 22:11:55 +0700

    Add project files.

---
+++ kisscambodiaTeam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kisscambodiaTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
